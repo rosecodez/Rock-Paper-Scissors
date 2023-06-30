1461ff1dd2d730440a4f55ba8f33fd67f50e4574
Rose <rosebeats09@gmail.com>
2023-06-29 23:27:43 -0400

    Create player prompt for obtaining value

--- script.js
+++ script.js
@@ -5,3 +5,23 @@ function getComputerChoice () {
     console.log(choices[(Math.floor(Math.random() * choices.length))]);
 }
 getComputerChoice()
+
+
+//player's choice
+let playerChoice = prompt("What is your choice?")
+
+if (playerChoice.toLowerCase() == "rock")
+    alert("You chose Rock");
+if (playerChoice.toLowerCase() == "paper")
+    alert("You chose Paper");
+if (playerChoice.toLowerCase() == "scissors")
+    alert("You chose Scissors");
+
+
+
+//round
+function playRound(playerSelection, computerSelection) {
+    if(playerSelection="Rock")
+    if(getComputerChoice = "Rock")
+        alert("Tie!")
+}

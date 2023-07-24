873676d555099f5639797c2b82a09a0f0aa5b837
Rose <rosebeats09@gmail.com>
2023-07-23 21:57:45 -0400

    Create UI

--- script.js
+++ script.js
@@ -1,93 +1,82 @@
-//Initial score values
-let playerScore = 0;
+//Score values
 let computerScore = 0;
+let playerScore = 0;
 
+// DOM elements
+const pScore = document.getElementById('playerScore');
+const cScore = document.getElementById('computerScore');
+const buttons = document.querySelectorAll('.selection button');
+const showIcon = document.querySelector('.show i');
+const computerShowIcon = document.querySelector('.computer i');
 
-//Computer's random choice function
-let computerSelection = getComputerChoice();
+//Choices
+const rockIcon = "rock";
+const paperIcon = "paper";
+const scissorsIcon = "scissors";
 
-function getComputerChoice (computerSelection) {
-    const choices = ["rock", "paper", "scissors"];
-    let choice = choices[Math.floor(Math.random()* choices.length)];
-    return choice;
+const randomClasses = [rockIcon, paperIcon, scissorsIcon];
+const text = document.getElementById('demo');
+
+//Results
+const tie = ()=>{
+    text.innerHTML = "It's a Tie ! ";
+    text.style.color = 'orange';
 }
 
-//Player's choice through prompt function
-function getPlayerChoice() {
-    let playerSelection = prompt("Rock, paper or scissors?");
-        if (playerSelection.toLowerCase() == "rock") {
-            console.log("You chose Rock");
-        } else if (playerSelection.toLowerCase() == "paper") {
-            console.log("You chose Paper");
-        } else if (playerSelection.toLowerCase() == "scissors") {
-            console.log("You chose Scissors");
-        } else {
-            alert("Try again please");
-        }
-        return playerSelection;
+const win = ()=>{
+    text.innerHTML = "You win!";
+    text.style.color = 'rgb(1, 146, 1)';
 }
 
-//function for playing rounds
-//comparison
-function playRound(playerSelection, computerSelection) {
-    if (playerSelection === "rock" && computerSelection === "paper") {
-        console.log("Computer wins!");
-        computerScore++;
-        return computerScore;
-    } else if (playerSelection === "scissors" && computerSelection === "paper") {
-        console.log("Player wins!");
-        playerScore++;
-        return playerScore;
-    } else if (playerSelection === "scissors" && computerSelection === "rock") {
-        console.log("Computer wins!");
-        computerScore++;
-        return computerScore;
-    } else if (playerSelection === "rock" && computerSelection === "scissors") {
-        console.log("Player wins!");
-        playerScore++;
-        return playerScore;
-    } else if (playerSelection === "rock" && computerSelection === "paper") {
-        console.log("Computer wins!");
-        computerScore++;
-        return computerScore;
-    } else if (playerSelection === "paper" && computerSelection === "rock") {
-        console.log("Player wins!");
-        playerScore++;
-        return playerScore;
-    } else if (playerSelection === "paper" && computerSelection === "scissors") {
-        console.log("Computer wins!");
-        computerScore++;
-        return computerScore;
-    } else {
-        console.log("Tie!");
-    }
+const lose = ()=>{
+    text.innerHTML = "Computer wins! ";
+    text.style.color = 'blue';
 }
-//Game function
-//Loops 5 rounds
-function game() {
-    for(let i=0; i<5; i++) {
-        const computerSelection = getComputerChoice();
-        const playerSelection = getPlayerChoice();
-        console.log("Computer chose " + `${computerSelection}`)
-        getComputerChoice (computerSelection);
-        playRound(playerSelection, computerSelection)
-    }
+
+// Game function
+const game = () =>{
+    buttons.forEach(btn =>{
+        btn.addEventListener('click',(e)=>{
+            // Computer's choice
+           let clickedBtn = e.target.className;
+           showIcon.className = clickedBtn;
+           let randomNum = Math.floor(Math.random() * randomClasses.length);
+           computerShowIcon.className = randomClasses[randomNum];
+            
+           //Comparisons
+           if(showIcon.className === computerShowIcon.className){
+               pScore.innerHTML = pScore.innerHTML;
+               cScore.innerHTML = cScore.innerHTML;
+               tie();
+           }
+           else if(showIcon.className === rockIcon && computerShowIcon.className === scissorsIcon){
+               pScore.innerHTML = playerScore;
+               playerScore++;
+               win();
+           }else if(showIcon.className === rockIcon && computerShowIcon.className === paperIcon){
+               cScore.innerHTML = computerScore;
+               computerScore++;
+               lose();
+           }else if(showIcon.className === paperIcon && computerShowIcon.className === scissorsIcon){
+               cScore.innerHTML = computerScore;
+               computerScore++;
+               lose();
+           }else if(showIcon.className === paperIcon && computerShowIcon.className === rockIcon){
+               pScore.innerHTML = playerScore;
+               playerScore++;
+               win();
+           }else if(showIcon.className === scissorsIcon && computerShowIcon.className === rockIcon){
+               cScore.innerHTML = computerScore;
+               computerScore++;
+               lose();
+           }else if(showIcon.className === scissorsIcon && computerShowIcon.className === paperIcon){
+               pScore.innerHTML = playerScore;
+               playerScore++;
+               win();
+           }
+        });
+    });
 }
-//Score function comparison
-function score() {
-    if(playerScore > computerScore){
-        console.log("Game ends. Player wins game!");
-    } else if (playerScore < computerScore) {
-        console.log("Game ends. Computer wins game!");
-    } else 
-        console.log("Game ends. It's a tie!")
-}
-//Calling the game function
+
+// Run the game function
 game();
-
-//Display final score
-console.log("Computer's final score is = " + computerScore )
-console.log("Player's final score is = " + playerScore)
-
-//Calling the score function
-score();
